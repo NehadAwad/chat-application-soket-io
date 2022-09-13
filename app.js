31e2eb0da761eda4157a17139718596bb96578b3
NehadAwad <nehadakash622@gmail.com>
2022-09-13 23:13:00 +0600

    basic structure done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { urlencoded } = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 dotenv.config();
@@ -9,4 +12,25 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(()=> console.log('database connection successfully'))
+.then(() => console.log('database connection successfully'))
+.catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engin", "ejs");
+
+//static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//cookie parser
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routing
+
+//error handling
+
+app.listen(process.env.PORT, () =>{
+    console.log(`app is listening to ${process.env.PORT}`);
+});
+
