bb66ce3f3af76aee7a8c77de18d2376bbca98d7e
NehadAwad <nehadakash622@gmail.com>
2022-09-14 23:58:20 +0600

    error checked

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -8,6 +8,7 @@ function errorHandler(err, req, res, next){
    res.locals.error = process.env.NODE_ENV === "development" ? err : { message: err.message };
 
    res.status(err.status || 500);
+   res.locals.test = "salfjsd";
 
    if(!res.locals.html){
         res.render("error", {
