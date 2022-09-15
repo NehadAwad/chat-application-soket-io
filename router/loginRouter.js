8d05ee96135267f7ccb8a9277dbba217653b9cb3
NehadAwad <nehadakash622@gmail.com>
2022-09-16 00:46:01 +0600

    new inbox middleware added

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router()
 const { getLogin} = require('../controller/loginController');
+const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
 
-router.get("/", getLogin);
+
+router.get("/", decorateHtmlResponse("Login"), getLogin);
 
 module.exports = router;
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { getUsers }= require("../controller/usersController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+
 
 router.get("/", getUsers);
 
